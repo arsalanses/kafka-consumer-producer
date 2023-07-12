6751733a46e6a67b1f253b82765fa0a6d9e184cc
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-07-12 11:49:37 +0330

    update requirements

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -6,17 +6,17 @@ import os
 input_topic = os.environ.get('KAFKA_TOPIC', 'input')
 output_topic = os.environ.get('KAFKA_TOPIC', 'output')
 
-kafka_server = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', 'kafka:9092')
+kafka_server = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
 producer = KafkaProducer(bootstrap_servers=[kafka_server])
 consumer = KafkaConsumer(input_topic, bootstrap_servers=[kafka_server])
 
-while True:
-    for message in consumer:
-        message_value = json.loads(message.decode('utf-8'))
-        input_date = datetime.fromisoformat(message_value['date'])
-        output_date = input_date.isoformat('T') + 'Z'
-        output_message = {'date': output_date}
-        producer.send(output_topic, json.dumps(output_message).encode('utf-8'))
-        print(f"Transformed message: {output_message}")
+for message in consumer:
+    message_value = json.loads(message.value.decode('utf-8'))
+    print(f"message: {message_value}")
+    input_date = datetime.fromisoformat(message_value['date'])
+    output_date = input_date.isoformat('T') + 'Z'
+    output_message = {'date': output_date}
+    print(f"Transformed message: {output_message}")
+    producer.send(output_topic, json.dumps(output_message).encode('utf-8'))
 
-# producer.flush()
+producer.flush()
--- producer/producer.py
+++ producer/producer.py
@@ -4,17 +4,14 @@ import json
 import time
 import os
 
-kafka_server = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', 'kafka:9092')
+kafka_server = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
 producer = KafkaProducer(bootstrap_servers=[kafka_server])
 
 topic = os.environ.get('KAFKA_TOPIC', 'input')
 
-while True:
-    message = {'date': datetime.now().isoformat()}
-    encoded_message = json.dumps(message).encode('utf-8')
-    producer.send(topic, encoded_message)
-    print(f"Sent message: {message}")
+message = {'date': datetime.now().isoformat()}
+encoded_message = json.dumps(message).encode('utf-8')
+producer.send(topic, encoded_message)
+print(f"Sent message: {message}")
 
-    time.sleep(2)
-
-# producer.flush()
+producer.flush()
