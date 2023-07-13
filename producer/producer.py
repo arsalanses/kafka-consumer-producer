be6bae8e13b92c995108cdf0679c2a0b27119f21
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-07-13 12:46:34 +0330

    add prometheus_client

--- producer/producer.py
+++ producer/producer.py
@@ -1,7 +1,15 @@
+# from prometheus_client import CollectorRegistry, Counter, push_to_gateway
 from kafka import KafkaProducer
 from datetime import datetime
 import json
 import os
+
+# messages_produced = Counter('my_kafka_app_messages_produced', 'Number of messages produced')
+
+# def produce_message():
+#     messages_produced.inc()
+
+# registry = CollectorRegistry()
 
 kafka_server = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
 producer = KafkaProducer(bootstrap_servers=[kafka_server])
@@ -11,6 +19,8 @@ topic = os.environ.get('KAFKA_TOPIC', 'input')
 message = {'date': datetime.now().isoformat()}
 encoded_message = json.dumps(message).encode('utf-8')
 producer.send(topic, encoded_message)
+# produce_message()
+# push_to_gateway('http://pushgateway:9091', job='my_kafka_app', registry=registry)
 print(f"Sent message: {message}")
 
 producer.flush()
